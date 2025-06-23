a4eaa36ca738a798ee70543bc584ca3f91db52c0
NikolajCupan <cupannikolaj@gmail.com>
2025-06-23 18:36:49 +0200

    Initial commit v2

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,9 +1,16 @@
 package org;
 
 import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import io.github.cdimascio.dotenv.Dotenv;
+import reactor.core.publisher.Mono;
 
 public class Main {
     public static void main(String[] args) {
-        DiscordClient client = DiscordClient.create("TOKEN");
+        Dotenv dotenv = Dotenv.load();
+
+        DiscordClient client = DiscordClient.create(dotenv.get("TOKEN"));
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
+        login.block();
     }
 }
