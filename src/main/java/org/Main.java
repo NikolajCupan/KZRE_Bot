ae16304c3942963e8f883256280bf0c8bba592a2
NikolajCupan <cupannikolaj@gmail.com>
2025-06-23 18:35:54 +0200

    Initial commit

---
+++ src/main/java/org/Main.java
@@ -0,0 +1,9 @@
+package org;
+
+import discord4j.core.DiscordClient;
+
+public class Main {
+    public static void main(String[] args) {
+        DiscordClient client = DiscordClient.create("TOKEN");
+    }
+}
