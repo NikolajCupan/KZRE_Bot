0daf58d78216932504434afbb41884d12e5e7dc9
NikolajCupan <cupannikolaj@gmail.com>
2025-06-26 18:17:44 +0200

    Changed Discord library

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,16 +1,9 @@
 package org;
 
-import discord4j.core.DiscordClient;
-import discord4j.core.GatewayDiscordClient;
 import io.github.cdimascio.dotenv.Dotenv;
-import reactor.core.publisher.Mono;
 
 public class Main {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
-
-        DiscordClient client = DiscordClient.create(dotenv.get("TOKEN"));
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
-        login.block();
     }
 }
