e4c61c9eaea784e4f0fe9a7d2aff0297f8e24f77
NikolajCupan <cupannikolaj@gmail.com>
2025-06-30 16:19:56 +0200

    Automatic database initialization script execution

---
+++ src/main/java/org/Constants.java
@@ -0,0 +1,8 @@
+package org;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Constants {
+    public static final Path RESOURCES_PATH = Paths.get("src", "main", "resources");
+}
--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -5,14 +5,21 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.action.Quote;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileReader;
 import java.text.MessageFormat;
 
 public class Main {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static Dotenv DOTENV;
-    public static SessionFactory SESSION_FACTORY;
+    public static SessionFactory DATABASE_SESSION_FACTORY;
 
     public static void main(String[] args) {
         Main.DOTENV = Dotenv.load();
@@ -22,7 +29,8 @@ public class Main {
                 Main.DOTENV.get("DATABASE_HOST"), Main.DOTENV.get("DATABASE_PORT"), Main.DOTENV.get("DATABASE_NAME")));
         configuration.setProperty("hibernate.hikari.dataSource.user", Main.DOTENV.get("DATABASE_USER"));
         configuration.setProperty("hibernate.hikari.dataSource.password", Main.DOTENV.get("DATABASE_PASSWORD"));
-        Main.SESSION_FACTORY = configuration.buildSessionFactory();
+        Main.DATABASE_SESSION_FACTORY = configuration.buildSessionFactory();
+        Main.initializeDatabase();
 
         JDA api = JDABuilder.createDefault(Main.DOTENV.get("TOKEN")).enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .build();
@@ -30,5 +38,25 @@ public class Main {
         MessagesListener messagesListener = new MessagesListener();
         api.addEventListener(messagesListener);
         messagesListener.registerActionHandler(new Quote());
+    }
+
+    public static void initializeDatabase() {
+        Session session = Main.DATABASE_SESSION_FACTORY.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.doWork(connection -> {
+            ScriptRunner scriptRunner = new ScriptRunner(connection);
+            scriptRunner.setSendFullScript(false);
+            scriptRunner.setStopOnError(true);
+
+            try {
+                scriptRunner.runScript(new FileReader(Constants.RESOURCES_PATH.resolve("schema.sql").toString()));
+            } catch (Exception ignore) {
+                Main.LOGGER.error("Could not run database initialization script");
+            }
+        });
+
+        transaction.commit();
+        session.close();
     }
 }
